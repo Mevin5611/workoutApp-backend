e8b04ccb5c06efd971b430caf3d7f6018741e1e9
Mevin5611 <mevinmevi88@gmail.com>
2024-04-02 00:05:18 +0530

    test

--- server.js
+++ server.js
@@ -28,8 +28,8 @@ app.use('/api/user/',userRoutes)
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
-    app.listen(process.env.PORT,()=>{
-        console.log("DB connected & running on PORT",process.env.PORT);
+    app.listen(4000,()=>{
+        console.log("DB connected & running on PORT",4000);
     })
 
 })
