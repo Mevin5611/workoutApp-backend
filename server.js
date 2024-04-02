7ed473995093c939b85470ac0efed675a0463055
Mevin5611 <mevinmevi88@gmail.com>
2024-04-02 15:07:50 +0530

    t

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose=require('mongoose')
 const workoutsroutes=require('./routes/workouts')
 const userRoutes=require('./routes/user')
 const cors = require('cors')
-const bodyParser = require("body-parser")
+
 
 // express app
 
@@ -19,7 +19,7 @@ app.use(express.json())
 
 //routes
 app.use(cors())
-app.use(bodyParser.json())
+
 app.use('/api/workouts/',workoutsroutes)
 app.use('/api/user/',userRoutes)
 
